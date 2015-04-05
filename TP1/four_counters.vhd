66093631a3cade04eb550be5e22fe7e936aff0c0
fedearm <fedeja@gmail.com>
2015-04-05 19:50:23 -0300

    ya para el informe

--- TP1/four_counters.vhd
+++ TP1/four_counters.vhd
@@ -2,14 +2,14 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity contador_4dig is
-    generic ( N : integer := 4);
+    generic ( M : integer := 4);
     Port ( clk : in  std_logic;
            rst : in  std_logic;
 	   en  : in  std_logic;
-           out0 : out  std_logic_vector (N-1 downto 0);
-           out1 : out  std_logic_vector (N-1 downto 0);
-           out2 : out  std_logic_vector (N-1 downto 0);
-           out3 : out  std_logic_vector (N-1 downto 0) );
+           out0 : out  std_logic_vector (M-1 downto 0);
+           out1 : out  std_logic_vector (M-1 downto 0);
+           out2 : out  std_logic_vector (M-1 downto 0);
+           out3 : out  std_logic_vector (M-1 downto 0) );
 end contador_4dig;
 
 architecture Behavioral of contador_4dig is
